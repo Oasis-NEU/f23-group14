d1b409b61c62614612fbb3c0c12c0e7cfae2195c
riyajroy23 <147739827+riyajroy@users.noreply.github.com>
2023-10-29 13:46:57 -0400

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import SignUp from "./pages/SignUp";
