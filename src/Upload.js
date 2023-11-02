64b263d326f35c29ebae3dddaca7d127505d2be3
lokyeyoung-create <141677588+lokyeyoung-create@users.noreply.github.com>
2023-11-02 12:14:55 -0400

    Update Upload.js

--- src/Upload.js
+++ src/Upload.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Upload() {
     return (
-        <div>
+        <div className="Upload">
             
         </div>
     )
+}
+
+function ClothingUpload() {
+    
+    const [file, setFile] = useState()
+
+    function handleChange(event) {
+        setFile(event.target.files[0])
+    }
+
+    return (
+        <div className="ClothingUpload">
+            <form>
+                <h1>Clothing File Upload</h1>
+                <input type="file" />
+                <button type="submit">Upload</button>
+            </form>
+        </div>
+    );
 }
 
