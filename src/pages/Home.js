a2c8d699979ef3247b1a355e7cad6cf3e4028fd8
lokyeyoung-create <141677588+lokyeyoung-create@users.noreply.github.com>
2023-11-12 12:49:09 -0500

    new stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
 
 export default function Home() {
-    return (
+  const [listings, setListings] = useState([]);
 
-        <div className="home-page">
-            <h1>Thrifting Exchange</h1>
+  useEffect(() => {
+    async function readAll() {
+      let { data, error } = await supabase.from("listings").select("*");
 
-     
-        </div>
-    )
+      console.log("listings: ", data);
+      setListings(data);
+    }
+    readAll();
+  }, []);
+
+  return (
+    <div className="home-page">
+      <h1>Thrifting Exchange</h1>
+
+      <div>
+        {listings.map((listing) => (
+          <p>{listing.item_description}</p>
+        ))}
+      </div>
+
+      <h1>Create listing</h1>
+      {/* item_description - text input */}
+      {/* button, click the button => insert a new listing, refresh the list */}
+    </div>
+  );
 }
